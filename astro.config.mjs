f9a77ddf12ad69606a675efb2132bc359de02f5f
Camleaf <bdaace09@gmail.com>
2026-04-23 12:20:58 -0400

    init

---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'My Docs',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			sidebar: [
+                {
+                    label: 'Getting Started',
+                    autogenerate: {directory: "getting-started"}
+                },
+                {
+                    label: 'Lessons',
+                    autogenerate: {directory: "Lessons"}
+                },
+			],
+		}),
+	],
+});
