910101743b1d7d173dee1d2387c50f1b0b818394
Camleaf <bdaace09@gmail.com>
2026-04-23 20:52:20 -0400

    feat: scaffold lessons 1.0, 2.0, 3.0, 4.0; create contributing.mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,17 +6,42 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Mechmania Docs',
+			social: [
+                { icon: 'github', label: 'GitHub', href: 'https://github.com/Mech-Mania/learning' }
+            ],
 			sidebar: [
                 {
                     label: 'Getting Started',
                     autogenerate: {directory: "getting-started"}
                 },
                 {
-                    label: 'Lessons',
-                    autogenerate: {directory: "Lessons"}
+                    label: 'lessons',
+                    items: [
+                        {
+                            label: "u1 intro",
+                            autogenerate: {directory: "lessons/u1-intro"},
+                        },
+                        {
+                            label: "u2 design",
+                            autogenerate: {directory: "lessons/u2-design"},
+                        },
+                        {
+                            label: "u3 electrical",
+                            autogenerate: {directory: "lessons/u3-electrical"},
+                        },
+                        {
+                            label: "u4 programming",
+                            autogenerate: {directory: "lessons/u4-programming"},
+                        },
+                    ]
                 },
+                {
+                    label: 'Guides',
+                    autogenerate: {directory: "guides"}
+                },
+
+
 			],
 		}),
 	],
