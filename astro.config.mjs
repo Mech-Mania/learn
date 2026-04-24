920d8cdd52f3b2a9b73be62e0c2055a57d9c1cb8
Camleaf <bdaace09@gmail.com>
2026-04-24 11:06:36 -0400

    feat: change styles and add mechmania logo (#4)

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,45 +4,50 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'Mechmania Docs',
-			social: [
-                { icon: 'github', label: 'GitHub', href: 'https://github.com/Mech-Mania/docs' }
-            ],
-			sidebar: [
-                {
-                    label: 'Getting Started',
-                    autogenerate: {directory: "getting-started"}
-                },
-                {
-                    label: 'lessons',
-                    items: [
-                        {
-                            label: "u1 intro",
-                            autogenerate: {directory: "lessons/u1-intro"},
-                        },
-                        {
-                            label: "u2 design",
-                            autogenerate: {directory: "lessons/u2-design"},
-                        },
-                        {
-                            label: "u3 electrical",
-                            autogenerate: {directory: "lessons/u3-electrical"},
-                        },
-                        {
-                            label: "u4 programming",
-                            autogenerate: {directory: "lessons/u4-programming"},
-                        },
-                    ]
-                },
-                {
-                    label: 'Guides',
-                    autogenerate: {directory: "guides"}
-                },
+  integrations: [
+      starlight({
+          title: 'Mechmania Docs',
+          social: [
+              { icon: 'github', label: 'GitHub', href: 'https://github.com/Mech-Mania/docs' }
+          ],
+          customCss: [
+              // Path to your Tailwind base styles:
+              './src/styles/global.css',
+          ],
+          sidebar: [
+              {
+                  label: 'Getting Started',
+                  autogenerate: {directory: "getting-started"}
+              },
+              {
+                  label: 'lessons',
+                  items: [
+                      {
+                          label: "U1 intro",
+                          autogenerate: {directory: "lessons/u1-intro"},
+                      },
+                      {
+                          label: "U2 design",
+                          autogenerate: {directory: "lessons/u2-design"},
+                      },
+                      {
+                          label: "U3 electrical",
+                          autogenerate: {directory: "lessons/u3-electrical"},
+                      },
+                      {
+                          label: "U4 programming",
+                          autogenerate: {directory: "lessons/u4-programming"},
+                      },
+                  ]
+              },
+              {
+                  label: 'Guides',
+                  autogenerate: {directory: "guides"}
+              },
 
 
-			],
-		}),
+          ],
+      }),
 	],
+
 });
